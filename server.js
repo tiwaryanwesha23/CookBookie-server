3ea66b426d0fe69af713316eec76b556b1a16043
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-09-14 10:08:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const cors = require('cors');
 const app = express();
 
 // Create a Redis client
-const redisClient = Redis.createClient({
-  url: "rediss://red-ck0vnfkojvrs73a6rcl0:IPOnaaiVqXjL4SMACRbTuuEJQbZhYi8U@singapore-redis.render.com:6379"
-});
+const redisClient = Redis.createClient(
+  {
+  url: "rediss://red-ck0vnfkojvrs73a6rcl0:IPOnaaiVqXjL4SMACRbTuuEJQbZhYi8U@singapore-redis.render.com:6379",
+  socket: {
+    tls: false,
+    rejectUnauthorized: false,
+  }
+}
+);
 
 redisClient.on('error', (error) => {
   console.error(`Redis Error: ${error}`);
